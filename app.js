fbab6b969530218bce3a3bda05bf68d021fb3330
Quertte <a.injen90@gmail.com>
2023-01-30 18:09:11 +0300

    test connection

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const { serverConfig } = require('./config/config');
+
+const { sequelize } = require('./db/models');
+
+const app = express();
+
+const PORT = process.env.PORT ?? 4000;
+
+serverConfig(app);
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully');
+    app.listen(PORT, () => console.log(`Server started at ${PORT} port`));
+  } catch (error) {
+    console.error('Unable to connect to the database', error);
+  }
+}
+
+testConnection();
